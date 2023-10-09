6fa457c03d05f9adc4b1bcd8d06d130b78ffe65e
hebertsanto <hebertsantos0704@gmail.com>
2023-10-08 22:06:36 -0300

    feat: added conditional rendering and change layout

--- src/pages/clients/index.tsx
+++ src/pages/clients/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import axios from 'axios';
-import { ContainerPagination, Table, TableClientsContainer } from './style';
-import { useNavigate } from 'react-router-dom';
+import { ContainerPagination, Table, TableClientsContainer, Container, Search, SubContainer, ButtonRegisterAClient } from './style';
+import { Link, useNavigate } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import { Client } from '../../core/types';
 import { useState } from 'react';
+
 
 export const Clients: React.FC = () => {
 
@@ -16,7 +17,7 @@ export const Clients: React.FC = () => {
       });
   });
 
-  const [ currentPage, setCurrentPage ]  = useState(1);
+  const [ currentPage, setCurrentPage ] = useState(1);
   const itemsPerPage = 8;
 
   const startIndex = (currentPage - 1) * itemsPerPage;
@@ -25,65 +26,87 @@ export const Clients: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    <TableClientsContainer>
-      <Table>
-        <thead>
-          <tr>
-            <th>
-              name
-            </th>
-            <th>
-              email
-            </th>
-            <th>
-              phone
-            </th>
-            <th>
-              cep
-            </th>
-            <th>
-              adress
-            </th>
-            <th>
-              actions
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {paginatedClients.length > 0 &&
-            paginatedClients.map((client: Client) =>
-              <>
-                <tr key={client._id}>
-                  <td>
-                    {client.name}
-                  </td>
-                  <td>{client.email}</td>
-                  <td>{client.phoneNumber}</td>
-                  <td>{client.cep}</td>
-                  <td>{client.address}</td>
-                  <td>
-                    <button onClick={() => navigate(`client/${client._id}`)}>actions</button>
-                  </td>
+    <>
+      {paginatedClients?.length == 0 || undefined ? <div>voce não tem nehum cliente cadastrado, <Link to='/dashboard/client/new'>registrar</Link></div> :
+        <div style={{ minHeight: '100vh' }}>
+          <Container>
+            <SubContainer>
+              <div>
+                <p>dashboard ynex</p>
+              </div>
+              <div>
+                <Search placeholder='search clients'/>
+                <ButtonRegisterAClient
+                  onClick={() => navigate('/dashboard/client/new')}
+                >
+                  register client
+                </ButtonRegisterAClient>
+              </div>
+            </SubContainer>
+          </Container>
+          <TableClientsContainer>
+            <Table>
+              <thead>
+                <tr>
+                  <th>
+                    name
+                  </th>
+                  <th>
+                    email
+                  </th>
+                  <th>
+                    phone
+                  </th>
+                  <th>
+                    cep
+                  </th>
+                  <th>
+                    adress
+                  </th>
+                  <th>
+                    actions
+                  </th>
                 </tr>
-              </>
-            )}
-        </tbody>
-      </Table>
-      <ContainerPagination>
-        <button
-          onClick={() => setCurrentPage(currentPage - 1)}
-          disabled={currentPage === 1}
-        >
-          prev page
-        </button>
-        <span>Página {currentPage}</span>
-        <button
-          disabled={endIndex >= data?.clients?.length}
-          onClick={() => setCurrentPage(currentPage + 1)}
-        >
-          next page
-        </button>
-      </ContainerPagination>
-    </TableClientsContainer>
+              </thead>
+              <tbody>
+                {paginatedClients.length > 0 &&
+                  paginatedClients.map((client: Client) =>
+                    <>
+                      <tr key={client._id}>
+                        <td>
+                          {client.name}
+                        </td>
+                        <td>{client.email}</td>
+                        <td>{client.phoneNumber}</td>
+                        <td>{client.cep}</td>
+                        <td>{client.address}</td>
+                        <td>
+                          <button onClick={() => navigate(`client/${client._id}`)}>actions</button>
+                        </td>
+                      </tr>
+                    </>
+                  )}
+              </tbody>
+            </Table>
+            <ContainerPagination>
+              <button
+                onClick={() => setCurrentPage(currentPage - 1)}
+                disabled={currentPage === 1}
+              >
+                prev page
+              </button>
+              <span>Página {currentPage}</span>
+              <button
+                disabled={endIndex >= data?.clients?.length}
+                onClick={() => setCurrentPage(currentPage + 1)}
+              >
+                next page
+              </button>
+            </ContainerPagination>
+          </TableClientsContainer>
+        </div>
+      }
+
+    </>
   );
 };
--- src/pages/clients/style.ts
+++ src/pages/clients/style.ts
@@ -47,3 +47,43 @@ export const ContainerPagination = styled.div`
   }
  }
 `;
+export const Container = styled.div`
+  min-height: 10vh;
+`;
+export const SubContainer = styled.div`
+  display: flex;
+  margin: 20px auto;
+  align-items: center;
+  justify-content:space-evenly;
+  width: 99vw;
+  div p{
+    margin-left: 30px;
+    font-family: 'Inter';
+    font-size: 1.5rem;
+  }
+`;
+
+export const Search = styled.input`
+  width: 500px;
+  height: 40px;
+  margin-left: 30px;
+  border: 1px solid gainsboro;
+  margin-top: 10px;
+  padding-left: 10px;
+  outline: none;
+
+  &:focus{
+    border: 1px solid rgba(0, 0, 0, 0.8);;
+  }
+`;
+export const ButtonRegisterAClient = styled.button`
+  width: 200px;
+  height: 40px;
+  margin-left: 50px;
+  background-color:  rgba(24, 120, 231, 0.8);
+  cursor: pointer;
+  color: #fff;
+  font-family: roboto;
+  font-size: 0.9rem;
+  border:none;
+`;
